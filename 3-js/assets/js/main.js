0ccafa612f3b52092f5e181e4442e358fb61e860
Daniela <danielalima.psicologa@gmail.com>
2023-08-06 11:59:15 -0300

    código final para o test sapient

--- 3-js/assets/js/main.js
+++ 3-js/assets/js/main.js
@@ -1,10 +1,29 @@
-// jQuery
-// $(document).ready(function() {
-  // code
-// });
+window.onload = function() {
+   let hamburguer = document.querySelector('.menu-button')
+   let menu = document.querySelector('.menu')
 
-// Vanilla JS
 
-// window.onload = function() {
-  // code
-// };
+   // open and close - click on button menu //
+    hamburguer.addEventListener('click',function(){
+      if(menu.classList.contains('-active')){
+         menu.classList.remove('-active')
+     }else{
+         menu.classList.add('-active')
+      }
+   })
+ 
+ // button - item sanfon //
+   let sanfon = document.querySelectorAll('.accordion .item')
+   sanfon.forEach(function(item){
+     item.addEventListener('click',function(){
+       if(item.classList.contains('-active')){
+          item.classList.remove('-active')
+      }else{
+          item.classList.add('-active')
+       }
+    }) 
+   })
+ 
+ 
+ };
+ 
